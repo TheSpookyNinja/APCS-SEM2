0b3e18f3b8cc892f27d67c32166a7ec2d8709699
Dean <2006584@student.acalanes.k12.ca.us>
2018-01-29 11:21:09 -0800

    Commit 2
    A 2-3

--- MagPie Lab/activity_3/MagpieRunner3.java
+++ MagPie Lab/activity_3/MagpieRunner3.java
@@ -1,18 +1,8 @@
 package activity_3;
-
 import java.util.Scanner;
 
-/**
- * A simple class to run the Magpie class.
- * @author Laurie White
- * @version April 2012
- */
 public class MagpieRunner3
 {
-
-	/**
-	 * Create a Magpie, give it user input, and print its replies.
-	 */
 	public static void main(String[] args)
 	{
 		Magpie3 maggie = new Magpie3();
@@ -21,11 +11,10 @@ public class MagpieRunner3
 		Scanner in = new Scanner (System.in);
 		String statement = in.nextLine();
 		
-		while (!statement.equals("Bye"))
+		while (!statement.equals("Bye") || !statement.equals("bye"))
 		{
 			System.out.println (maggie.getResponse(statement));
 			statement = in.nextLine();
 		}
 	}
-
 }
